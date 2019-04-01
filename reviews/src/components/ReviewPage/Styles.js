2bb8c00a48b8959b8fb2aa12989652c14e236e92
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-31 23:51:28 -0600

    Build Mobile Responsive Style for ReviewPage.

--- reviews/src/components/ReviewPage/Styles.js
+++ reviews/src/components/ReviewPage/Styles.js
@@ -4,7 +4,7 @@ import { flexContainer } from '../../globalStyles/Styles';
 
 // Review Page Components
 export const ReviewContainer = styled.section`
-  margin-top:  30px;
+  margin-top: 30px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -19,20 +19,30 @@ export const Rating = styled(flexContainer)`
   background-color: rgba(53, 23, 6, 0.5);
   color: rgb(81,197,212);;
   font-weight: bold;
-  margin-right: 1.5rem;
   align-self: center;
 
   span {
     font-size: 5.2rem;
   }
 
+  @media only screen and (max-width: 650px) {
+    width: 160px;
+    height: 160px;
 
-  @media only screen and (max-width: 450px) {
-    width: 8rem;
     span {
-      font-size: 3.3rem;
+      font-size: 4.2rem;
     }
   }
+
+  @media only screen and (max-width: 450px) {
+    width: 140px;
+    height: 140px;
+
+    span {
+      font-size: 3.2rem;
+    }
+  }
+
 `;
 
 export const Author = styled.h1`
@@ -40,13 +50,19 @@ export const Author = styled.h1`
   font-weight: bold;
   margin-top: 30px;
   align-self: center;
-  text-decoration: underline;+
+  text-decoration: underline;
+  
+  @media only screen and (max-width: 650px) {
+    font-size: 2rem;
+    margin-top: 30px;
+  }
+
+  @media only screen and (max-width: 450px) {
+    font-size: 1.5rem;
+    margin-top: 15px;
+  }
 `
-export const ReviewDate = styled.p`
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-top: 30px;
-`
+
 export const Quote = styled.blockquote`
   max-width: 600px;
   font-style: italic;
@@ -54,6 +70,31 @@ export const Quote = styled.blockquote`
   padding-left: 30px;
   border-left: 10px solid rgb(105,68,37);
   margin: 30px 0;
+
+  @media only screen and (max-width: 650px) {
+    font-size: 2rem;
+    margin: 20px 10px;
+  }
+
+  @media only screen and (max-width: 450px) {
+    font-size: 1.5rem;
+    margin: 10px 10px;
+  }
+`
+export const ReviewDate = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-top: 30px;
+
+  @media only screen and (max-width: 650px) {
+    font-size: 1rem;
+    margin: 20px 10px;
+  }
+
+  @media only screen and (max-width: 450px) {
+    font-size: .75rem;
+    margin: 10px 10px;
+  }
 `
 
 export const Stars = styled.div`
@@ -62,8 +103,12 @@ export const Stars = styled.div`
   overflow-x: hidden;
   width: ${props => ((props.rating / 5) * 100) + "%"} // Calculates the number of stars to show.
 
+  @media only screen and (max-width: 650px) {
+    font-size: 1.25rem;
+  }
+
   @media only screen and (max-width: 450px) {
-    font-size: .7rem;
+    font-size: 1rem;
   }
 `;
 
@@ -81,5 +126,13 @@ export const ReturnToList = styled.button`
 
   &:hover {
     background-color: rgba(254,197,107,0.5);
+  }
+
+  @media only screen and (max-width: 650px) {
+    margin-top: 30px;
+  }
+
+  @media only screen and (max-width: 450px) {
+    margin-top: 15px;
   }
 `
